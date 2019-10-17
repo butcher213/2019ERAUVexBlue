6872b509535b70c419b8dd4dfbf2c254de089bf8
danielstruck <struckd@my.erau.edu>
2019-10-16 17:41:26 -0700

    added small robot and big robot folders

---
+++ Robot Big/src/Robot.cpp
@@ -0,0 +1,174 @@
+#include "Robot.hpp"
+
+//*********************************
+// Drive train constant definitions
+//*********************************
+const int Robot::MOVE_FORWARD = 127; // TODO check value correct
+const int Robot::MOVE_BACKWARD = -Robot::MOVE_FORWARD;
+const int Robot::STRAFE_RIGHT = 127; // TODO check value correct
+const int Robot::STRAFE_LEFT = -Robot::STRAFE_RIGHT;
+const int Robot::ROTATE_CW = 127; // TODO check value correct
+const int Robot::ROTATE_CCW = -Robot::ROTATE_CW;
+
+//***********************************
+// Tray actuator constant definitions
+//***********************************
+const int Robot::TRAY_RAISE = 127; // TODO check value correct
+const int Robot::TRAY_LOWER = -Robot::TRAY_RAISE;
+
+//**************************
+// Lift constant definitions
+//**************************
+const int Robot::LIFT_RAISE = 127; // TODO check value correct
+const int Robot::LIFT_LOWER = -Robot::LIFT_RAISE;
+
+//****************************
+// Intake constant definitions
+//****************************
+const int Robot::INTAKE_PULL = 127; // TODO check value correct
+const int Robot::INTAKE_PUSH = -Robot::INTAKE_PULL;
+
+//***************************
+// Singleton robot definition
+//***************************
+Robot Robot::robot_singleton = Robot();
+
+
+// Returns the robot singleton
+Robot Robot::singleton() {
+    return robot_singleton;
+}
+
+// returns the sign of the value
+int sign(double value) {
+    return (int) (value / abs(value));
+}
+
+//*********************************
+// Drive train function definitions
+//*********************************
+void Robot::forward(int velocity) {
+    if (abs(velocity) >= DRIVE_MINIMUM_VOLTAGE) {
+        frontLeft.move(velocity);
+        frontRight.move(velocity);
+        backLeft.move(velocity);
+        backRight.move(velocity);
+    }
+}
+void Robot::strafe(int velocity) {
+    if (abs(velocity) >= DRIVE_MINIMUM_VOLTAGE) {
+        frontLeft.move(velocity);
+        frontRight.move(-velocity);
+        backLeft.move(-velocity);
+        backRight.move(velocity);
+    }
+}
+void Robot::rotate(int velocity) {
+    if (abs(velocity) >= DRIVE_MINIMUM_VOLTAGE) {
+        frontLeft.move(velocity);
+        frontRight.move(-velocity);
+        backLeft.move(velocity);
+        backRight.move(-velocity);
+    }
+}
+void Robot::drive(int forwardVal, int strafeVal, int rotateVal) {
+    frontLeft.move(forwardVal + strafeVal + rotateVal);
+    frontRight.move(forwardVal - strafeVal - rotateVal);
+    backLeft.move(forwardVal - strafeVal + rotateVal);
+    backRight.move(forwardVal + strafeVal - rotateVal);
+}
+
+//****************************************
+// Autonomous control function definitions
+//****************************************
+void Robot::forwardDistance(double inches) {
+    double target = inches * ENCODER_COUNTS_PER_INCH;
+    int velocity = sign(inches) * Robot::MOVE_FORWARD;
+
+    forward(velocity);
+
+    while (abs(getDriveEncoderValue() - target) < ENCODER_TARGET_RANGE)
+        pros::delay(5);
+
+    forward(-velocity);
+    pros::delay(getDriveVelocity() * DRIVE_VELOCITY_TO_BRAKE_TIME_MS);
+    forward(0);
+}
+void Robot::strafeDistance(double inches) {
+    double target = inches * ENCODER_COUNTS_PER_INCH;
+    int velocity = sign(inches) * Robot::MOVE_FORWARD;
+
+    strafe(velocity);
+
+    while (abs(getDriveEncoderValue() - target) < ENCODER_TARGET_RANGE)
+        pros::delay(5);
+
+    strafe(-velocity);
+    pros::delay(getDriveVelocity() * DRIVE_VELOCITY_TO_BRAKE_TIME_MS);
+    strafe(0);
+}
+void Robot::rotateDegrees(double degrees) {
+    double target = degrees * ENCODER_COUNTS_PER_DEGREE;
+    int velocity = sign(degrees) * Robot::MOVE_FORWARD;
+
+    rotate(velocity);
+
+    while (abs(getDriveEncoderValue() - target) < ENCODER_TARGET_RANGE)
+        pros::delay(5);
+
+    rotate(-velocity);
+    pros::delay(getDriveVelocity() * DRIVE_VELOCITY_TO_BRAKE_TIME_MS);
+    rotate(0);
+}
+
+//**********************************
+// Drive sensor function definitions
+//**********************************
+double Robot::getDriveEncoderValue() {
+    double averageMagnitude = (abs(frontLeft.get_position())
+                             + abs(frontRight.get_position())
+                             + abs(backLeft.get_position())
+                             + abs(backRight.get_position())) / 4;
+
+    int signProduct = sign(frontLeft.get_position())
+                    * sign(frontRight.get_position())
+                    * sign(backLeft.get_position())
+                    * sign(backRight.get_position());
+
+    return signProduct * averageMagnitude;
+}
+double Robot::getDriveVelocity() {
+    double averageMagnitude = (abs(frontLeft.get_actual_velocity())
+                             + abs(frontRight.get_actual_velocity())
+                             + abs(backLeft.get_actual_velocity())
+                             + abs(backRight.get_actual_velocity())) / 4;
+
+    int signProduct = sign(frontLeft.get_actual_velocity())
+                    * sign(frontRight.get_actual_velocity())
+                    * sign(backLeft.get_actual_velocity())
+                    * sign(backRight.get_actual_velocity());
+
+    return signProduct * averageMagnitude;
+}
+
+//***********************************
+// Tray actuator function definitions
+//***********************************
+void Robot::actuateTray(int velocity) {
+    actuator.move(velocity);
+}
+
+//**************************
+// Lift function definitions
+//**************************
+void Robot::liftVelocity(int velocity) {
+    lift.move(velocity);
+}
+
+//****************************
+// Intake function definitions
+//****************************
+void Robot::intakeVelocity(int velocity) {
+    intakeLeft.move(velocity);
+    intakeRight.move(velocity);
+}
---
+++ Robot Big/src/initialize.cpp
@@ -0,0 +1,125 @@
+#include <cstring>
+#include <sstream>
+#include <cmath>
+#include "main.h"
+#include "Robot.hpp"
+
+
+static pros::Controller primary_controller(pros::E_CONTROLLER_MASTER);
+static int lcdMenuSelection = 0;
+
+void on_center_button() {
+    switch (lcdMenuSelection) {
+        case 0:
+        break;
+    }
+}
+
+void on_right_button() {
+    lcdMenuSelection++;
+}
+
+void on_left_button() {
+    lcdMenuSelection--;
+}
+
+/**
+ * Runs initialization code. This occurs as soon as the program is started.
+ *
+ * All other competition modes are blocked by initialize; it is recommended
+ * to keep execution time for this mode under a few seconds.
+ */
+// void mapJoyInput(int x, int y) {
+    // float max = std::max(std::abs(x), std::abs(y));
+    //
+    // if (max != 0) {
+    //     float r = std::sqrt(x*x + y*y);
+    //     int x_ = r / max * x;
+    //     int y_ = r / max * y;
+    //
+    //     printf(" | %4d  %4d", x_, y_);
+    //     if (r > 127) {
+    //         std::cout << "<";
+    //     }
+    //     else {
+    //         std::cout << " ";
+    //     }
+    // }
+    // else {
+    //     std::cout << " |    -     - ";
+    // }
+// }
+void initialize() {
+	pros::lcd::initialize();
+	pros::lcd::set_text(1, "Hello PROS User!");
+
+	pros::lcd::register_btn0_cb(on_left_button);
+	pros::lcd::register_btn1_cb(on_center_button);
+	pros::lcd::register_btn2_cb(on_right_button);
+
+
+    // static const int START = -127;
+    // static const int STOP = 127;
+    // static const float STEP = 127/8;
+
+    // std::cout << "   \\ X";
+    // for (float x = START; x <= STOP; x += STEP)
+    //     printf("%7d       ", (int) x);
+    // std::cout << "  Y \\" << std::endl;
+    //
+    // for (float y = START; y <= STOP; y += STEP) {
+    //     printf("%4d", (int) y);
+    //     for (float x = START; x <= STOP; x += STEP) {
+    //         mapJoyInput(x, y);
+    //     }
+    //     std::cout << std::endl << std::endl;
+    // }
+
+
+    /*while (1) {
+        float x = primary_controller.get_analog(pros::E_CONTROLLER_ANALOG_LEFT_X);
+        float y = primary_controller.get_analog(pros::E_CONTROLLER_ANALOG_LEFT_Y);
+        float r = sqrt(x*x + y*y);
+
+        float max = std::max(std::abs(x), std::abs(y));
+
+        if (max != 0) {
+            pros::delay(1000);
+            // float maxX = x / max * 127;
+            // float maxY = y / max * 127;
+
+            // float newX = r/127 * maxX;
+            // float newY = r/127 * maxY;
+
+            // simplified version
+            float x_ = r / max * x;
+            float y_ = r / max * y;
+
+            std::ostringstream str1;
+            std::ostringstream str2;
+            str1 << "x: " << x_;
+            str2 << "y: " << y_;
+
+            pros::lcd::set_text(0, str1.str());
+            pros::lcd::set_text(1, str2.str());
+        }
+    }*/
+}
+
+/**
+ * Runs while the robot is in the disabled state of Field Management System or
+ * the VEX Competition Switch, following either autonomous or opcontrol. When
+ * the robot is enabled, this task will exit.
+ */
+void disabled() {}
+
+/**
+ * Runs after initialize(), and before autonomous when connected to the Field
+ * Management System or the VEX Competition Switch. This is intended for
+ * competition-specific initialization routines, such as an autonomous selector
+ * on the LCD.
+ *
+ * This task will exit when the robot is enabled and autonomous or opcontrol
+ * starts.
+ */
+void competition_initialize() {}
